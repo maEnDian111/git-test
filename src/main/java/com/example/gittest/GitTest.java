b8e33c5103d6e109a40e30a7f2709dc797eb8696
MaSang <18739990251@163.com>
2022-04-06 15:17:14 +0800

    gitee test

--- src/main/java/com/example/gittest/GitTest.java
+++ src/main/java/com/example/gittest/GitTest.java
@@ -17,5 +17,6 @@ public class GitTest {
         System.out.println("hot-fix test2!");
         System.out.println("push test1!");
         System.out.println("pull test!");
+        System.out.println("giTee test!");
     }
 }
