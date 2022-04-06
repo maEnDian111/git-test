ec05bdf9edd7ba3eeba08e790a1a4f066c8be7a6
MaSang <18739990251@163.com>
2022-04-06 14:03:29 +0800

    first commit

---
+++ src/main/java/com/example/gittest/GitTest.java
@@ -0,0 +1,12 @@
+package com.example.gittest;
+
+/**
+ * @author StopDecadence
+ * @create 2022-04-06 13:59
+ * @email 3026174424@qq.com
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
